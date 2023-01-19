58340eca125e256c0a613ff2443cff37ec588281
Dharmendra <dharmendrasingh75039@gmail.com>
2023-01-19 20:16:10 +0530

    Adding middleware and modifying middleware

--- src/index.js
+++ src/index.js
@@ -1,12 +1,35 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { legacy_createStore as createStore } from "redux";
+import { legacy_createStore as createStore, applyMiddleware } from "redux";
 
 import "./index.css";
 import App from "./components/App";
 import rootReducer from "./reducers";
 
-const store = createStore(rootReducer);
+//function logger(obj, next, action)
+//logger(obj)(next)(action)
+//this obj is not a store it is a simple object with two keys {dispatch, getState}
+
+// const logger = function ({ dispatch, getState }) {
+//   return function (next) {
+//     return function (action) {
+//       //middleware code
+//       console.log("ACTION_TYPE", action.type);
+//       next(action);
+//     };
+//   };
+// };
+
+const logger =
+  ({ dispatch, getState }) =>
+  (next) =>
+  (action) => {
+    //logger code
+    console.log("ACTION_TYPE", action.type);
+    next(action);
+  };
+
+const store = createStore(rootReducer, applyMiddleware(logger));
 console.log("store", store);
 // console.log("before state", store.getState());
 
