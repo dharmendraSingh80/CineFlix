cccd292297aa96acc98dad01f33e88b5abccf056
Dharmendra <dharmendrasingh75039@gmail.com>
2023-01-20 19:26:03 +0530

    using omdb API & thunk & Adding search result in store

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { legacy_createStore as createStore, applyMiddleware } from "redux";
+import thunk from "redux-thunk";
 
 import "./index.css";
 import App from "./components/App";
@@ -25,11 +26,25 @@ const logger =
   (next) =>
   (action) => {
     //logger code
-    console.log("ACTION_TYPE", action.type);
+    if (typeof action !== "function") {
+      console.log("ACTION_TYPE", action.type);
+    }
     next(action);
   };
 
-const store = createStore(rootReducer, applyMiddleware(logger));
+// const thunk =
+//   ({ dispatch, getState }) =>
+//   (next) =>
+//   (action) => {
+//     //logger code
+//     if (typeof action === "function") {
+//       action(dispatch);
+//       return;
+//     }
+//     next(action);
+//   };
+
+const store = createStore(rootReducer, applyMiddleware(logger, thunk));
 console.log("store", store);
 // console.log("before state", store.getState());
 
